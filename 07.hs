03a56eab6b5ddec1b98467370eb2ddd0218b6a1d
Marcin Belicki <m.belicki.426@studms.ug.edu.pl>
2021-05-01 22:08:55 +0200

    6 corrected, 7 and 8 added

--- 07.hs
+++ 07.hs
@@ -1,8 +1,10 @@
 
+-- rekursywnie
 fib_r 0 = 0
 fib_r 1 = 1
 fib_r n = fib_r (n-1) + fib_r (n - 2)
 
+--iteracyjnie
 fib n = fib_help n 1 0 where
     fib_help 0 _ acc2 = acc2
     fib_help 1 acc1 _ = acc1
