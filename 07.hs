aca1b266162a925168d8ae41fb6ce18337b7a112
Marcin Belicki <m.belicki.426@studms.ug.edu.pl>
2021-05-28 19:41:31 +0200

    17, 18, 19 done

--- 07.hs
+++ 07.hs
@@ -4,7 +4,7 @@ fib_r 0 = 0
 fib_r 1 = 1
 fib_r n = fib_r (n-1) + fib_r (n - 2)
 
---iteracyjnie
+-- iteracyjnie
 fib n = fib_help n 1 0 where
     fib_help 0 _ acc2 = acc2
     fib_help 1 acc1 _ = acc1
